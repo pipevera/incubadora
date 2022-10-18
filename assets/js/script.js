44cbe3b54438bf4e5e16f4c936708363eca54ccf
Felipe <felipevera10@gmail.com>
2022-10-18 19:57:24 -0300

    comentado jquery

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+/* $(function() {
+  $("a").click(function(event){
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hashtag = this.hash;
+
+      $("html, body").animate({
+        scrollTop: ($(hashtag).offset().top - '100'
+      }, 500, function(){
+        window.location.hash = hashtag;
+      });
+    }
+  })
+}) */
